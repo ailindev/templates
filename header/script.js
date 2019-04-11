712cbe1e4eab83a6b5dc7c2bb3d9e2c121e86a57
ailindev <ailin.webdeveloper@gmail.com>
2019-04-11 20:24:56 +0500

    add menu and btn scroll down

--- header/script.js
+++ header/script.js
@@ -2,11 +2,30 @@ $(function () {
 
    // Hamburger
    function hamburger() {
-      let hamburger = $('.hamburger');
+      let hamburger = $('.hamburger'),
+          menu = $('.navigation');
       hamburger.on('click', function () {
          $(this).toggleClass('is-active');
+         // Menu toggle visible
+
+         // for mobile
+         if ($(window).width() < 768) {
+            if (hamburger.hasClass('is-active')) {
+               menu.css('display', 'flex').hide().slideDown('200');
+            } else {
+               menu.slideUp('200');
+            }
+         } else { // for tablet+desktop
+            if (hamburger.hasClass('is-active')) {
+               menu.css('display', 'flex').hide().fadeIn('200');
+            } else {
+               menu.fadeOut('200');
+            }
+         }
+
       })
    }
+
    hamburger();
 
 
